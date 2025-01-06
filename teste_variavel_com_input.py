9d319b377b4175fc3434c5b9b563e8ba3ddf2191
Dayanne Mendes <daymendesdev@gmail.com>
2025-01-06 18:28:34 -0300

    inclusão de calculadora e aula de variáveis

---
+++ teste_variavel_com_input.py
@@ -0,0 +1,16 @@
+nome = input("Nome: ")
+sobrenome = input("Sobrenome: ")
+idade = input("Idade: ")
+sexo = input("Sexo (masculino/feminino): ")
+
+if sexo.lower() == "feminino": #usei o .lower para garantir que a comparação funcione, mesmo que seja digitaddo com letras maiúsculas ou minúsculas
+    print(f"Bem-vinda, senhorita {nome}")
+else:
+    print(f"Bem-vindo, senhor {nome}")
+
+comida = input("O que gostaria de comer hoje? temos(cariru/lagosta/canguejo/maniçoba/tacacá)")
+
+
+        
+
+        
