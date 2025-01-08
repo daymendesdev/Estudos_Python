4785098d668e1c732a4e4dcf4e15de6b616ad321
Dayanne Mendes <daymendesdev@gmail.com>
2025-01-08 20:27:00 -0300

    Add files via upload

--- collections_listas_e_tuples.py
+++ collections_listas_e_tuples.py
@@ -20,6 +20,71 @@ print(familia[0:3])
 familia[1] = "linda da Day"
 print(familia)
 
-#para adicionar outro valor a lista
+#para adicionar uma outra lista(complemento) à sua lista
 familia.extend(["José","Ivete"])
 print(familia)
+
+#para adicionar apenas um valor a lista
+familia.append("Ana Carolina")
+print(familia)
+
+#para escolher em qual índice você vai adicionar o valor a lista
+familia.insert(1,"Dadadayyy")
+print(familia)
+
+#Remove o último elemento da lista por padrão.
+familia.pop() 
+
+#escolher qual vai remover
+familia.remove("Dadadayyy")
+print(familia)
+
+#apagar todos os registro da lista
+#familia.clear()
+#print(familia)
+
+#retornar o indice com base no valor
+print(familia.index("Yasmim"))
+
+#Para saber quantos valores iguais eu tenho dentro de uma lista
+print(familia.count("Yasmim"))
+
+idades = [28,26,24,22,20,18,11,59,49]
+#para ordenar uma lista de inteiros na ordem ascendente(se eu fizer isso com a lista de strings, ele vai retornar em ordem alfabética)
+idades.sort()
+print(idades)
+
+#para inverter uma lista
+familia.reverse()
+print(familia)
+
+#O .sort seguido do .reverse retorna o inverso em ordem
+
+#para creferenciar uma lista para outra variável
+familia2 = familia
+print(familia)
+
+#para fazer uma copia da lista( diferente do modelo acima, no modelo a seguir, se você alterar algo na variável de origem, essa continua a mesma)
+familia2 = familia.copy()
+
+
+
+##############################
+
+                ###TUPLES###
+
+#A diferença entre a lista e o tuple, é que o tuple é inalterável, ou seja, não pode ser alterado.
+
+#exemplos
+#nesse exemplo é uma list, vamos remover um índice da mesma
+coordenadas = [-54.1022, 129.0681]
+coordenadas.pop()
+print(coordenadas)
+
+#agora veremos um tuple
+#nesse, se eu usar algum metodo de remover ou adicionar, ele dará erro
+#todas as outras funções que não alteram, removem ou adicionam são válidas
+#coordenadas2 = (-54.1022, 129.0681)
+#coordenadas2.pop()
+#print(coordenadas2)
+
